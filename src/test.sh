8ca6b2d1fb77f2aae93521bd30c12693b1a0a7d9
koki <k.t.the-answer@hotmail.co.jp>
2022-12-04 14:19:11 +0900

    test.sh

--- src/test.sh
+++ src/test.sh
@@ -5,7 +5,7 @@ wget --no-check-certificate https://figshare.com/ndownloader/files/38344040 \
 -O data/vaccine_tensor.npy
 
 # Perform tensorlycv
-docker run -v $(pwd):/work ghcr.io/kokitsuyuzaki/tensorlycv:main \
--i /work/data/vaccine_tensor.npy -o /work/output \
+docker run ghcr.io/kokitsuyuzaki/tensorlycv:main \
+-i data/vaccine_tensor.npy -o output \
 --cores=2 --rank=2 --trials=2 --iters=2 \
 --ratio=30 --memgb=100
