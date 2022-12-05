8645502b2c29fc53236b9a6a67b21270125605c0
koki <k.t.the-answer@hotmail.co.jp>
2022-12-05 19:10:12 +0900

    feat: Tests on GitHub Actions

--- src/test.sh
+++ src/test.sh
@@ -5,7 +5,7 @@ wget --no-check-certificate https://figshare.com/ndownloader/files/38344040 \
 -O data/vaccine_tensor.npy
 
 # Perform tensorlycv
-docker run -it --rm -v $(pwd):/work ghcr.io/kokitsuyuzaki/tensorlycv:main \
+docker run --rm -v $(pwd):/work ghcr.io/kokitsuyuzaki/tensorlycv:main \
 -i /work/data/vaccine_tensor.npy -o /work/output \
 --cores=2 --rank=2 --trials=2 --iters=2 \
 --ratio=30 --memgb=100
