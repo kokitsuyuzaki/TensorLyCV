46fbbea47079d3184649c68d109ca793237f4fa9
koki <k.t.the-answer@hotmail.co.jp>
2023-01-06 15:38:44 +0900

    dag

--- src/report.sh
+++ src/report.sh
@@ -1,3 +1,4 @@
 # HTML
 mkdir -p report
-snakemake --report report/tensorlycv.html --config input=data/vaccine_tensor.npy outdir=output
+snakemake --report report/tensorlycv.html --config input=data/vaccine_tensor.npy outdir=output \
+rank=2 trials=2 iters=2 ratio=30
